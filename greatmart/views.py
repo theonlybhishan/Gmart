13e72caf36284e9b855da466e58e5d3d17b797db
theonlybhishan <bhishanpoudel17@gmail.com>
2021-06-14 13:11:15 +0545

    first commit

---
+++ greatmart/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,redirect
+from store.models import Product
+def index(request):
+    products = Product.objects.all().filter(is_available =True)
+
+    context={
+        'products':products,
+    }
+    return render(request, 'home.html',context)
